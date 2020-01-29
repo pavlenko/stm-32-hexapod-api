91a2b430497f78d29b17924c44ffb95afe6ffe48
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2020-01-29 11:03:10 +0200

    add main

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by master on 29.01.20.
+//
+
+#include "main.h"
+
+int main()
+{
+    while (true) {}
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by master on 29.01.20.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
