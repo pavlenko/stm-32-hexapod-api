451a23e8dc082c249a290c8fad7d1013c0b1958c
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2020-02-08 19:58:42 +0200

    add control related files

---
+++ src/V2/control.h
@@ -0,0 +1,22 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//TODO define controls state & set state+recalculate functions: setDirection(...)
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported function prototypes --------------------------------------------- */
+/* Exported variables --------------------------------------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CONTROL_H
