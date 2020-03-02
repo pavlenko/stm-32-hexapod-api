2c39fb64f447f547e03c5676e529e00a40433d23
Eugene Pavlenko <eugene.p@doubledmarketing.com>
2020-03-02 15:27:27 +0200

    update spider leg points structure

--- lib/PE_SpiderV2/src/PE_SpiderV2.h
+++ lib/PE_SpiderV2/src/PE_SpiderV2.h
@@ -7,12 +7,12 @@ extern "C" {
 
 #include <stdint.h>
 
-typedef enum PE_SpiderV2_LegMode_t {
+typedef enum PE_SpiderV2_LegMode_s {
     PE_SPIDER_V2_LEG_MODE_GROUNDED,
     PE_SPIDER_V2_LEG_MODE_FLOATING,
 } PE_SpiderV2_LegMode_t;
 
-typedef enum PE_SpiderV2_LegPos_t {
+typedef enum PE_SpiderV2_LegPos_s {
     PE_SPIDER_V2_LEG_POS_FL,
     PE_SPIDER_V2_LEG_POS_FR,
     PE_SPIDER_V2_LEG_POS_ML,
@@ -21,36 +21,26 @@ typedef enum PE_SpiderV2_LegPos_t {
     PE_SPIDER_V2_LEG_POS_BR,
 } PE_SpiderV2_LegPos_t;
 
-typedef struct PE_SpiderV2_t PE_SpiderV2_t;
+typedef struct PE_SpiderV2_s PE_SpiderV2_t;
 
-typedef struct PE_SpiderV2_Point2D_t {
+typedef struct PE_SpiderV2_Point2D_s {
     float x;
     float y;
 } PE_SpiderV2_Point2D_t;
 
-typedef struct PE_SpiderV2_Point3D_t {
+typedef struct PE_SpiderV2_Point3D_s {
     float x;
     float y;
     float z;
 } PE_SpiderV2_Point3D_t;
 
-typedef struct PE_SpiderV2_LegConfig_t {
-    PE_SpiderV2_Point3D_t mount;
-    PE_SpiderV2_Point3D_t target;
-} PE_SpiderV2_LegConfig_t;
-
-typedef struct PE_SpiderV2_LegTarget_t {
-    PE_SpiderV2_Point3D_t target;
-    uint16_t angles[3];
-} PE_SpiderV2_LegTarget_t;
-
-typedef struct PE_SpiderV2_remote_t {
+typedef struct PE_SpiderV2_remote_s {
     float moveX;
     float moveY;
     int8_t rotateZ;
 } PE_SpiderV2_remote_t;
 
-typedef struct PE_SpiderV2_moving_t {
+typedef struct PE_SpiderV2_moving_s {
     uint16_t height;
     float moveByX;
     float moveByY;
@@ -59,14 +49,15 @@ typedef struct PE_SpiderV2_moving_t {
 
 typedef void (*PE_SpiderV2_StateHandler_t) ();
 
-typedef struct PE_SpiderV2_State_t {
+typedef struct PE_SpiderV2_State_s {
     PE_SpiderV2_StateHandler_t onEntering;
     PE_SpiderV2_StateHandler_t onDispatch;
 } PE_SpiderV2_State_t;
 
-typedef struct PE_SpiderV2_t {
-    PE_SpiderV2_LegConfig_t legConfig[6];
-    PE_SpiderV2_LegTarget_t legTarget[6];
+typedef struct PE_SpiderV2_s {
+    PE_SpiderV2_Point2D_t legMounts[6];
+    PE_SpiderV2_Point2D_t legSources[6];
+    PE_SpiderV2_Point2D_t legTargets[6];
     PE_SpiderV2_remote_t remote;
     PE_SpiderV2_moving_t moving;
     PE_SpiderV2_State_t *prevState;
