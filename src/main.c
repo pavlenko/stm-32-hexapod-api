c75c49b4e307b38348cbfe9844265c09cb10a657
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2020-03-30 10:15:09 +0300

    attach all motors

--- src/main.c
+++ src/main.c
@@ -13,13 +13,27 @@ typedef struct {
 TIM_HandleTypeDef TIM_Handle;
 
 PE_Servo180_Timer_t timer1;
+PE_Servo180_Timer_t timer2;
+PE_Servo180_Timer_t timer3;
 
-PE_Servo180_Motor_t motor1 = {.ID = 0, .reverse = 0};
-PE_Servo180_Motor_t motor2 = {.ID = 1, .reverse = 0};
-PE_Servo180_Motor_t motor3 = {.ID = 2, .reverse = 0};
-PE_Servo180_Motor_t motor4 = {.ID = 3, .reverse = 0};
-PE_Servo180_Motor_t motor5 = {.ID = 4, .reverse = 0};
-PE_Servo180_Motor_t motor6 = {.ID = 5, .reverse = 0};
+PE_Servo180_Motor_t motor01 = {.ID = 0, .reverse = 0};
+PE_Servo180_Motor_t motor02 = {.ID = 1, .reverse = 0};
+PE_Servo180_Motor_t motor03 = {.ID = 2, .reverse = 0};
+PE_Servo180_Motor_t motor04 = {.ID = 3, .reverse = 0};
+PE_Servo180_Motor_t motor05 = {.ID = 4, .reverse = 0};
+PE_Servo180_Motor_t motor06 = {.ID = 5, .reverse = 0};
+PE_Servo180_Motor_t motor07 = {.ID = 6, .reverse = 0};
+PE_Servo180_Motor_t motor08 = {.ID = 7, .reverse = 0};
+PE_Servo180_Motor_t motor09 = {.ID = 8, .reverse = 0};
+PE_Servo180_Motor_t motor10 = {.ID = 9, .reverse = 0};
+PE_Servo180_Motor_t motor11 = {.ID = 10, .reverse = 0};
+PE_Servo180_Motor_t motor12 = {.ID = 11, .reverse = 0};
+PE_Servo180_Motor_t motor13 = {.ID = 12, .reverse = 0};
+PE_Servo180_Motor_t motor14 = {.ID = 13, .reverse = 0};
+PE_Servo180_Motor_t motor15 = {.ID = 14, .reverse = 0};
+PE_Servo180_Motor_t motor16 = {.ID = 15, .reverse = 0};
+PE_Servo180_Motor_t motor17 = {.ID = 16, .reverse = 0};
+PE_Servo180_Motor_t motor18 = {.ID = 17, .reverse = 0};
 
 Motor_Pin_t motorPins[] = {
     {GPIOA, 0},
@@ -28,6 +42,20 @@ Motor_Pin_t motorPins[] = {
     {GPIOA, 3},
     {GPIOA, 4},
     {GPIOA, 5},
+
+    {GPIOA, 6},
+    {GPIOA, 7},
+    {GPIOA, 8},
+    {GPIOA, 9},
+    {GPIOA, 10},
+    {GPIOA, 11},
+
+    {GPIOB, 3},
+    {GPIOB, 4},
+    {GPIOB, 5},
+    {GPIOB, 6},
+    {GPIOB, 7},
+    {GPIOB, 8},
 };
 
 PE_Button_Key_t key1;
@@ -56,12 +84,29 @@ int main()
     MX_TIM_PWM_Init(TIM4, &TIM_Handle);
 
     PE_Servo180_createTimer(&timer1);
-    PE_Servo180_attachMotor(&timer1, &motor1);
-    PE_Servo180_attachMotor(&timer1, &motor2);
-    PE_Servo180_attachMotor(&timer1, &motor3);
-    PE_Servo180_attachMotor(&timer1, &motor4);
-    PE_Servo180_attachMotor(&timer1, &motor5);
-    PE_Servo180_attachMotor(&timer1, &motor6);
+    PE_Servo180_createTimer(&timer2);
+    PE_Servo180_createTimer(&timer3);
+
+    PE_Servo180_attachMotor(&timer1, &motor01);
+    PE_Servo180_attachMotor(&timer1, &motor02);
+    PE_Servo180_attachMotor(&timer1, &motor03);
+    PE_Servo180_attachMotor(&timer1, &motor04);
+    PE_Servo180_attachMotor(&timer1, &motor05);
+    PE_Servo180_attachMotor(&timer1, &motor06);
+
+    PE_Servo180_attachMotor(&timer2, &motor07);
+    PE_Servo180_attachMotor(&timer2, &motor08);
+    PE_Servo180_attachMotor(&timer2, &motor09);
+    PE_Servo180_attachMotor(&timer2, &motor10);
+    PE_Servo180_attachMotor(&timer2, &motor11);
+    PE_Servo180_attachMotor(&timer2, &motor12);
+
+    PE_Servo180_attachMotor(&timer3, &motor13);
+    PE_Servo180_attachMotor(&timer3, &motor14);
+    PE_Servo180_attachMotor(&timer3, &motor15);
+    PE_Servo180_attachMotor(&timer3, &motor16);
+    PE_Servo180_attachMotor(&timer3, &motor17);
+    PE_Servo180_attachMotor(&timer3, &motor18);
 
     HAL_TIM_Base_Start_IT(&TIM_Handle);
     HAL_TIM_PWM_Start_IT(&TIM_Handle, TIM_CHANNEL_1);
@@ -100,13 +145,13 @@ void PE_SpiderV2_refreshOnEntering(PE_SpiderV2_t *spider) {
 }
 
 void PE_SpiderV2_refreshOnComplete(PE_SpiderV2_t *spider) {
-    PE_Servo180_setRadian(&motor1, spider->legs[PE_SPIDER_V2_LEG_POS_FL].cDegree);
-    PE_Servo180_setRadian(&motor2, spider->legs[PE_SPIDER_V2_LEG_POS_FL].fDegree);
-    PE_Servo180_setRadian(&motor3, spider->legs[PE_SPIDER_V2_LEG_POS_FL].tDegree);
+    PE_Servo180_setRadian(&motor01, spider->legs[PE_SPIDER_V2_LEG_POS_FL].cDegree);
+    PE_Servo180_setRadian(&motor02, spider->legs[PE_SPIDER_V2_LEG_POS_FL].fDegree);
+    PE_Servo180_setRadian(&motor03, spider->legs[PE_SPIDER_V2_LEG_POS_FL].tDegree);
 
-    PE_Servo180_setRadian(&motor4, spider->legs[PE_SPIDER_V2_LEG_POS_FR].cDegree);
-    PE_Servo180_setRadian(&motor5, spider->legs[PE_SPIDER_V2_LEG_POS_FR].fDegree);
-    PE_Servo180_setRadian(&motor6, spider->legs[PE_SPIDER_V2_LEG_POS_FR].tDegree);
+    PE_Servo180_setRadian(&motor04, spider->legs[PE_SPIDER_V2_LEG_POS_FR].cDegree);
+    PE_Servo180_setRadian(&motor05, spider->legs[PE_SPIDER_V2_LEG_POS_FR].fDegree);
+    PE_Servo180_setRadian(&motor06, spider->legs[PE_SPIDER_V2_LEG_POS_FR].tDegree);
 }
 
 void PE_Servo180_setTimerOverflow(PE_Servo180_Timer_t *timer, uint16_t value) {
@@ -151,14 +196,21 @@ void MX_GPIO_Init() {
 
     /* GPIO clock enable */
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOC_CLK_ENABLE();
 
     /* GPIO Configuration */
-    GPIO_InitStruct.Pin   = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
+    GPIO_InitStruct.Pin   = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11;
     GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin   = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     GPIO_InitStruct.Pin   = GPIO_PIN_15;
     GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
