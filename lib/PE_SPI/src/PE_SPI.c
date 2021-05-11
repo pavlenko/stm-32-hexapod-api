3eb0775126c2b6f3cf1acbe52a2bd391b9435ad1
Eugene Pavlenko <eugene.p@doubledmarketing.com>
2021-05-11 16:30:47 +0300

    add code

--- lib/PE_SPI/src/PE_SPI.c
+++ lib/PE_SPI/src/PE_SPI.c
@@ -1,1 +1,24 @@
 #include "PE_SPI.h"
+
+#include <stddef.h>
+
+void PE_SPI_send(PE_SPI_Device_t *device, uint8_t *data, uint16_t size)
+{
+    PE_SPI_select(device);
+
+    device->txBuffer = data;
+    device->txBuffer = data;
+    device->txBuffer = data;
+
+    device->rxBuffer = (uint8_t *)NULL;
+    device->txBuffer = 0U;
+    device->txBuffer = 0U;
+
+    //TODO weak send implementation callback
+}
+
+__attribute__((weak))
+void PE_SPI_select(PE_SPI_Device_t *spi)
+{
+    (void) spi;
+}
--- lib/PE_SPI/src/PE_SPI.h
+++ lib/PE_SPI/src/PE_SPI.h
@@ -7,11 +7,27 @@ extern "C" {
 
 #include <stdint.h>
 
+//TODO enum
+#define PE_SPI_MODE0 0b00
+#define PE_SPI_MODE1 0b01
+#define PE_SPI_MODE2 0b10
+#define PE_SPI_MODE3 0b11
+
+//TODO enum
+#define PE_SPI_MSB_FIRST 0U
+#define PE_SPI_LSB_FIRST 1U
+
 //TODO SS pin abstraction
 //TODO transmit/receive logic with interrupt based and blocking modes
 //TODO separate configuration structure?
 //TODO master/slave mode? maybe separate types
 //TODO interrupt like callbacks?
+
+typedef struct PE_SPI_Config_s {
+    uint32_t baud;
+    uint8_t bitOrder;
+    uint8_t dataMode;
+} PE_SPI_Config_t;
 
 typedef struct PE_SPI_Device_s {
     uint8_t *txBuffer;
@@ -22,8 +38,13 @@ typedef struct PE_SPI_Device_s {
     uint16_t rxBufferCount;
 } PE_SPI_Device_t;
 
+void PE_SPI_send(PE_SPI_Device_t *device, uint8_t *data, uint16_t size);
+void PE_SPI_read(PE_SPI_Device_t *device, uint8_t *data, uint16_t size);
+
 void PE_SPI_onTXComplete(PE_SPI_Device_t *spi);
 void PE_SPI_onRXComplete(PE_SPI_Device_t *spi);
+
+void PE_SPI_select(PE_SPI_Device_t *spi);
 
 #ifdef __cplusplus
 }
