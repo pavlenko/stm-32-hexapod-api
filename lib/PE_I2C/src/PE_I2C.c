0e2d93216a6087c7b55434b037c77e5fcb5613eb
Eugene Pavlenko <eugene.p@doubledmarketing.com>
2021-09-23 16:29:19 +0300

    try to add code

--- lib/PE_I2C/src/PE_I2C.c
+++ lib/PE_I2C/src/PE_I2C.c
@@ -1,2 +1,31 @@
 #include "PE_I2C.h"
 
+#include <stddef.h>
+#include <stdint.h>
+
+PE_I2C_Status_t PE_I2C_open(PE_I2C_Driver_t *driver, PE_I2C_Device_t *device) {
+    if (driver->device != NULL) {
+        return PE_I2C_STATUS_ERROR;
+    }
+    if (driver->state != PE_I2C_STATE_READY) {
+        return PE_I2C_STATUS_BUSY;
+    }
+
+    driver->device = device;
+
+    return PE_I2C_STATUS_OK;
+}
+
+PE_I2C_Status_t PE_I2C_wait(PE_I2C_Driver_t *driver) {
+    //TODO maybe check timeout here & pass value for compare
+    return PE_I2C_STATUS_OK;
+}
+
+PE_I2C_Status_t PE_I2C_stop(PE_I2C_Driver_t *driver) {
+    if (driver->state != PE_I2C_STATE_READY) {
+        return PE_I2C_STATUS_BUSY;
+    }
+
+    driver->device = NULL;
+    return PE_I2C_STATUS_OK;
+}
--- lib/PE_I2C/src/PE_I2C.h
+++ lib/PE_I2C/src/PE_I2C.h
@@ -15,12 +15,12 @@ typedef enum {
 } PE_I2C_Status_t;
 
 typedef enum {
-    I2C_STATE_READY   = 0x00U,
-    I2C_STATE_LISTEN  = 0x01U,
-    I2C_STATE_BUSY_RX = 0x02U,
-    I2C_STATE_BUSY_TX = 0x03U,
-    I2C_STATE_SUCCESS = 0x04U,
-    I2C_STATE_FAILURE = 0x05U,
+    PE_I2C_STATE_READY   = 0x00U,
+    PE_I2C_STATE_LISTEN  = 0x01U,
+    PE_I2C_STATE_BUSY_RX = 0x02U,
+    PE_I2C_STATE_BUSY_TX = 0x03U,
+    PE_I2C_STATE_SUCCESS = 0x04U,//TODO move to result?
+    PE_I2C_STATE_FAILURE = 0x05U,//TODO move to result?
 } PE_I2C_State_t;
 
 typedef struct {
@@ -37,29 +37,29 @@ typedef struct {
     void *hw;
     uint32_t speed;
     PE_I2C_Device_t *device;
-    PE_I2C_State_t status;
+    PE_I2C_State_t state;
 } PE_I2C_Driver_t;
 
 // Master functions
-PE_I2C_Status_t PE_I2C_open(PE_I2C_Device_t *dev);// configure bus
-PE_I2C_Status_t PE_I2C_wait(PE_I2C_Driver_t *dev);// wait operation completed
-PE_I2C_Status_t PE_I2C_stop(PE_I2C_Driver_t *dev);// un-configure device (need?)
+PE_I2C_Status_t PE_I2C_open(PE_I2C_Driver_t *driver, PE_I2C_Device_t *device);
+PE_I2C_Status_t PE_I2C_wait(PE_I2C_Driver_t *driver);
+PE_I2C_Status_t PE_I2C_stop(PE_I2C_Driver_t *driver);
 
-PE_I2C_Status_t PE_I2C_send(PE_I2C_Driver_t *dev, uint8_t *data, uint16_t size);// send data in async mode
-PE_I2C_Status_t PE_I2C_read(PE_I2C_Driver_t *dev, uint8_t *data, uint16_t size);// read data in async mode
+PE_I2C_Status_t PE_I2C_send(PE_I2C_Driver_t *driver, uint8_t *data, uint16_t size);// send data in async mode
+PE_I2C_Status_t PE_I2C_read(PE_I2C_Driver_t *driver, uint8_t *data, uint16_t size);// read data in async mode
 
-PE_I2C_Status_t PE_I2C_setMem(PE_I2C_Driver_t *dev, uint16_t *addr, uint8_t addrSize, uint8_t *data, uint8_t size);// send data internal memory address
-PE_I2C_Status_t PE_I2C_getMem(PE_I2C_Driver_t *dev, uint16_t *addr, uint8_t addrSize, uint8_t *data, uint8_t size);// read data internal memory address
+PE_I2C_Status_t PE_I2C_setMem(PE_I2C_Driver_t *driver, uint16_t *addr, uint8_t addrSize, uint8_t *data, uint8_t size);// send data internal memory address
+PE_I2C_Status_t PE_I2C_getMem(PE_I2C_Driver_t *driver, uint16_t *addr, uint8_t addrSize, uint8_t *data, uint8_t size);// read data internal memory address
 
 // Slave functions
-void PE_I2C_listenTo(PE_I2C_Driver_t *dev);// init slave mode with specific address + configure bus
+void PE_I2C_listenTo(PE_I2C_Driver_t *driver);// init slave mode with specific address + configure bus
 void PE_I2C_onReceive();// call when master use send mode
 void PE_I2C_onRequest();// call when master use read mode
 
 // Interrupt handlers
-void PE_I2C_TX_interrupt(PE_I2C_Driver_t *dev);
-void PE_I2C_RX_interrupt(PE_I2C_Driver_t *dev);
-void PE_I2C_ER_interrupt(PE_I2C_Driver_t *dev);
+void PE_I2C_TX_interrupt(PE_I2C_Driver_t *driver);
+void PE_I2C_RX_interrupt(PE_I2C_Driver_t *driver);
+void PE_I2C_ER_interrupt(PE_I2C_Driver_t *driver);
 
 //TODO callbacks
 
